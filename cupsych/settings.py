18548be64ede4ef1659cae25f13627bbef894dfd
onyuwn <jakeherman@outlook.com>
2023-03-04 14:24:17 -0600

    Settings.

--- cupsych/settings.py
+++ cupsych/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+g+=*@r^6$30$t9cqhj^-7=mdj21019gq_pxry_wdrnl2prqrr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cupsychedelics.azurewebsites.net']
 
 
 # Application definition
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = "/psychsymp/static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
